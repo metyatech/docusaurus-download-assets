aefcfa818e15f48ae379ac83a506cf0ba042ecc4
metyatech <metyatech@gmail.com>
2026-01-06 23:30:06 +0900

    create functionality

---
+++ index.js
@@ -0,0 +1,18 @@
+module.exports = function assetResourcePlugin() {
+  return {
+    name: 'docusaurus-download-assets',
+    configureWebpack() {
+      return {
+        module: {
+          rules: [
+            {
+              include: [/[/\\]files[/\\]/],
+              exclude: /\.(js|jsx|ts|tsx|json|md|mdx)$/i,
+              type: 'asset/resource',
+            },
+          ],
+        },
+      };
+    },
+  };
+};
